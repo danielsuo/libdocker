cd525c12be62d80275b5ad6e8f6d699c16497446
Daniel Suo <danielsuo@gmail.com>
2017-02-27 20:36:43 -0500

    Make library compatible with c++

--- src/docker.h
+++ src/docker.h
@@ -11,6 +11,10 @@
 #include <string.h>
 #include <curl/curl.h>
 
+#ifdef __cplusplus
+extern "C"{
+#endif
+
 struct buffer {
   char *data;
   size_t size;
@@ -29,5 +33,9 @@ int docker_destroy(DOCKER *docker_client);
 char *docker_buffer(DOCKER *docker_client);
 CURLcode docker_post(DOCKER *docker_client, char *url, char *data);
 CURLcode docker_get(DOCKER *docker_client, char *url);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //DOCKER_DOCKER_H
